6e38e56c4603e063208df94070d03daa824c26df
Britta <brittan.villegas@gmail.com>
2023-02-17 12:57:34 +0100

    Added linkedin and github links to db and router

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,13 @@ const fetchMob = async () => {
   const getMembers = await fetch(`http://localhost:3000/mobs/1/members`);
   const memberCards = await getMembers.json()
   memberCards.forEach((member : Member) => {
-    mobContainer!.innerHTML += `<article><div class="mob__card"><h2 class="mob__item">${member.name}</h2></div></article>`
+    mobContainer!.innerHTML += `
+    <article>
+      <div class="mob__card">
+        <h2 class="mob__item">${member.name}</h2>
+        
+      </div>
+    </article>`
   } )
 }
 
